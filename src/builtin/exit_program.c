33616011658bca2603e31c32ca4ca9b0f590e46f
marcon <florian.marcon@epitech.eu>
2018-04-30 16:26:34 +0200

    print message error about exit builtin

--- src/builtin/exit_program.c
+++ src/builtin/exit_program.c
@@ -5,6 +5,7 @@
 ** exit
 */
 
+#include <stdio.h>
 #include "header_shell.h"
 #include "my.h"
 
@@ -13,7 +14,13 @@ int	exit_program(shell_t *shell, char **cmd)
 	if (len_tab(cmd) > 2)
 		return (1);
 	shell->exit = true;
-	if (len_tab(cmd) == 2)
-		shell->value_exit = my_getnbr(cmd[1]);
+	if (len_tab(cmd) == 2) {
+		if (my_str_isnum(cmd[1]))
+			shell->value_exit = my_getnbr(cmd[1]);
+		else if (atoi(cmd[1]) < 0)
+			fprintf(stderr, "exit: Badly formed number.\n");
+		else
+			fprintf(stderr, "exit: Expression Syntax.\n");
+	}
 	return (0);
 }
