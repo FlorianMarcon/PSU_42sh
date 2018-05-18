7e33a68755f0c5e46cfd1863d9a8ccddf5e9cde1
paulvitry <paul.vitry@epitech.eu>
2018-05-18 14:39:32 +0200

    env setenv unsetenv

--- src/utilitaries/generate_shell.c
+++ src/utilitaries/generate_shell.c
@@ -20,7 +20,7 @@ unsigned int	generate_shell(char **envp, shell_t *shell)
 	char *str = NULL;
 
 	shell->arr_env = envp;
-	shell->list_env = generate_list_env(envp);
+	shell->list_env = init_env(envp);
 	shell->env = generate_hm_env(shell->list_env);
 	getcwd(shell->pwd, sizeof(shell->pwd));
 	shell->old_pwd = NULL;
